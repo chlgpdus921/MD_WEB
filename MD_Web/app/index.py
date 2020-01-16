e3a6eb7a32880cae0d94f1b2bd9889a6c4dda61a
Hye-Yeon Choi <chlgpdus921@naver.com>
2020-01-15 16:07:02 -0800

    0105COMMIT

---
+++ MD_Web/app/index.py
@@ -0,0 +1,30 @@
+from flask import render_template, request
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/login')
+def index(state = None):
+    return render_template('/login.html', LoginState = "None")
+
+@app.route('/main', methods=['POST', 'GET'])
+def loginResult():
+    if request.method =='POST':
+        id = request.form['id']
+        password = request.form['password']
+        print(id)
+        print(password)
+
+        try:
+            if id == 'admin' and password == '12345':
+                return render_template('/index.html')
+            else:
+               return render_template('/login.html', LoginState = "NO")
+
+        except:
+            return 'You are not registered'
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
--- MD_Web2/app/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-# 추가할 모듈이 있다면 추가
-
-app = Flask(__name__, template_folder='templates')
-
-# 파일 이름이 index.py이므로
-from app import index
-app.run(debug=True)
